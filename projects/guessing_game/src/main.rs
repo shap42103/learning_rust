83b6ac6d5c990a0ad8e4c09cd64a50221fefa350
shap42103 <nakamura.keigo13@gmail.com>
2025-11-08 10:30:33 +0900

    chapter2: 数当て完成

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,21 +1,48 @@
 // クレート: コンパイル単位。本ファイルは実行可能プログラム。他にはライブラリクレートもある。
-use std::io;
+use rand::Rng;
+use std::{cmp::Ordering, io, os::fd::OwnedFd};
+
 // prelude: 標準ライブラリの一部はuseせずに使うことができる
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_number);
 
-    // letは不変。mutで可変になる。
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
+        // letは不変。mutで可変になる。
+        let mut guess = String::new();
 
-    // &は参照を示す
-    // expectはread_lineの結果がErrの時の処理。ここではクラッシュさせる
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // &は参照を示す
+        // expectはread_lineの結果がErrの時の処理。ここではクラッシュさせる
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // プレースホルダ {}
-    println!("You guessed: {}", guess);
+        // 変数の再定義は許容される
+        // u32: 符号なし32ビット整数. parseするためには型注釈が必須
+        // parseはResultを返すのでmatchで処理する
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            }
+        };
+
+        // プレースホルダ {}
+        println!("You guessed: {}", guess);
+
+        // cmp: 比較
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
